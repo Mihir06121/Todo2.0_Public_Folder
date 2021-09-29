80945d757dae1c4767d5f32acef54addb105b1e1
mihir Panchal <mp06121@gmail.com>
2021-09-29 23:50:05 +0530

    landing page 50% done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Section1 from "./components/Section1"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Section1.js
@@ -0,0 +1,72 @@
+import React,  {useEffect} from 'react'
+import lander from '../ss/logo.png'
+import loader from '../ss/loader.png'
+import teamCreate from '../ss/teamcreate.png'
+import todoCreate from '../ss/todocreate.png'
+import AOS from 'aos'
+const Section1 = () => {
+
+    AOS.init({
+        duration:'1200'
+    })
+    return (
+        <div className="p-3 container-fluid">
+            <div className="container p-5"><h1 className="text-fluid display-md-1" data-aos="fade-down">PRESENTING TO YOU</h1></div>
+            <div className="row shadow-lg p-5">
+                <div className="pt-4 pb-4 col-sm d-flex 
+                    justify-content-center align-items-center" style={{borderRight: "10px solid #0275d8"}} data-aos="fade-up" data-aos-delay="600">
+                    <img className="p-5" src={lander} height="400px" alt="" />
+                </div>
+                <div className="col-sm d-flex 
+                    justify-content-center align-items-center" data-aos="fade-left" data-aos-delay="600">
+                    <h1 className="display-1">TODO 2.0<br/><h1>0.1.0</h1></h1>
+                </div>
+            </div>
+            {/* <div className=" shadow-lg p-5">
+                <h1 className="p-2">Not a usual Todo App.</h1>
+                <h1 className="p-2">Create Team and Assign Tasks.</h1>
+            </div> */}
+            <div>
+                <div className="row p-5">
+                    <div className="col-sm shadow-lg" >
+                        <img className="img-fluid" src={loader} size="auto" alt="" />
+                    </div>
+                    <div className="col-sm d-flex 
+                        justify-content-center align-items-center">
+                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
+                            <h1 className="display-4">Not an usual Todo App.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="row p-5">
+                    <div className="col-sm shadow-lg" >
+                        <img className="img-fluid p-5" src={teamCreate} size="auto" alt="" />
+                    </div>
+                    <div className="col-sm d-flex order-lg-first
+                        justify-content-center align-items-center">
+                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
+                            <h1 className="display-4">Create Teams</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="row p-5">
+                    <div className="col-sm shadow-lg" >
+                        <img className="img-fluid p-5" src={todoCreate} size="auto" alt="" />
+                    </div>
+                    <div className="col-sm d-flex
+                        justify-content-center align-items-center">
+                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
+                            <h1 className="display-4">Assign tasks to individual team members</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section1
