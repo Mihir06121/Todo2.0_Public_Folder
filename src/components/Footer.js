741ac3a5583ed7148f58fd13bc3b5bab54bbe159
mihir Panchal <mp06121@gmail.com>
2021-09-30 18:57:09 +0530

    UI changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="d-flex justify-content-center align-items-center">
             <div className="shadow-lg p-5">
-                <h1>DESIGNED AND DEVELOPED BY <span className="text-primary">MIHIR PANCHAL</span></h1>
+                <h3>DESIGNED AND DEVELOPED BY <a href="https://www.mihirpanchal.com" target="_blanck"><span className="text-primary">MIHIR PANCHAL</span></a></h3>
             </div>
         </div>
     )
--- src/components/Section1.js
+++ src/components/Section1.js
@@ -17,7 +17,7 @@ const Section1 = () => {
     // valid till OCT29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
     return (
         <div className="p-3">
-            <div className="container p-5"><h1 className="text-fluid display-md-1" data-aos="fade-down">PRESENTING TO YOU</h1></div>
+            <div className="container pt-5 pb-5"><h1 className="display-4" data-aos="fade-down">PRESENTING TO YOU</h1></div>
             <div className="row shadow-lg">
                 <div className="pt-4 pb-4 col-sm d-flex 
                     justify-content-center align-items-center" style={{borderRight: "10px solid #0275d8"}} data-aos="fade-up" data-aos-delay="600">
@@ -25,26 +25,16 @@ const Section1 = () => {
                 </div>
                 <div className="col-sm d-flex 
                     justify-content-center align-items-center" data-aos="fade-left" data-aos-delay="600">
-                    <h1 className="display-1"><span className="text-primary">TODO</span> 2.0<br/><h2>beta version <span className="text-primary">0.1.0</span></h2></h1>
+                    <h1 className="display-1"><span className="text-primary"><span className="text-primary">TODO</span></span> 2.0<br/><h2>beta version <span className="text-primary">0.1.0</span></h2></h1>
                 </div>
             </div>
             <div className="pt-5 pb-5">
             <Button color="primary" onClick={toggle}><h1 className="display-4">Download</h1></Button>
                 <Modal isOpen={modal} toggle={toggle} size='lg' scrollable={true} className="">
-                    <ModalHeader toggle={toggle}>TODO 2.0 download</ModalHeader>
+                    <ModalHeader toggle={toggle}><span className="text-primary">TODO</span> 2.0 download</ModalHeader>
                         <ModalBody>
                             <div className="text-left">
                                 <h1>This application is in its beta version(0.1.0).</h1>
-                                {/* <h1>Following are few update that would be included in future beta versions.</h1>
-                                <ul className="p-3">
-                                    <li><h3>Socket.io for live discussion.</h3></li>
-                                    <li><h3>Implementing group discussion</h3></li>
-                                    <li><h3>Implementing email verification.</h3></li>
-                                    <li><h3>Pdf sharing.</h3></li>
-                                    <li><h3>List of users based on contacts list instead of displaying every user from DB.</h3></li>
-                                    <li><h3>Improve in local notification and server notifications.</h3></li>
-                                    <li><h3>Bugs fix and imprive stability.</h3></li>
-                                </ul> */}
                                 <div className="pt-5 pb-5">
                                     <h2>Report any bugs or crashes, your review on this application 
                                         is helpfull for me to provide you with more efficiant and stable application.</h2>
@@ -68,30 +58,36 @@ const Section1 = () => {
                 </Modal>
             </div>
             <div className="">
-                <div className="row p-5">
+                <div className="row p-lg-5">
                     <div className="col-sm shadow-lg rounded-lg" data-aos="fade-up">
                         <img className="img-fluid" src={loader} size="auto" alt="" />
                     </div>
-                    <div className="col-sm d-flex 
-                        justify-content-center align-items-center" data-aos="zoom-in-left" data-aos-delay="700 ">
-                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
-                            <h1 className="display-4 text-left">Not an usual Todo App.</h1>
+                    <div className="col-sm d-flex pt-5
+                        justify-content-center align-items-center" data-aos="zoom-in-left">
+                        <div className="p-lg-5 p-1" style={{borderBottom:"10px solid #0275d8", borderRight:"10px solid #0275d8", borderBottomRightRadius: 40}}>
+                            <h1 className="display-4 text-left">Not an usual <span className="text-primary">TODO</span> App.</h1>
                         </div>
                     </div>
                 </div>
             </div>
+            <div>
+                <hr className="m-5 bg-dark"/>
+            </div>
             <div className="">
-                <div className="row p-5">
-                    <div className="col-sm shadow-lg rounded-lg" data-aos="fade-up" data-aos-delay="700" >
+                <div className="row p-lg-5">
+                    <div className="col-sm shadow-lg rounded-lg" data-aos="fade-up">
                         <img className="img-fluid p-sm-5" src={teamCreate} size="auto" alt="" />
                     </div>
                     <div className="col-sm d-flex order-lg-first
-                        justify-content-center align-items-center" data-aos="fade-right" data-aos-delay="700" >
-                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
-                            <h1 className="display-4 text-left">TODO 2.0 comes with unique idea of creating teams</h1>
+                        justify-content-center align-items-center" data-aos="fade-right">
+                        <div className="p-lg-5 p-1" style={{borderBottom:"10px solid #0275d8", borderLeft:"10px solid #0275d8", borderBottomLeftRadius: 40}}>
+                            <h1 className="display-4 text-left"><span className="text-primary">TODO</span> 2.0 comes with unique idea of creating teams</h1>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div>
+                <hr className="m-5 bg-dark"/>
             </div>
             <div className="container pt-5 pb-5 text-left" data-aos="zoom-in-right">
                 <div style={{borderLeft: '10px solid #0275d8'}}>
@@ -100,34 +96,43 @@ const Section1 = () => {
                     </h2>
                 </div>
             </div>
+            <div>
+                <hr className="m-5 bg-dark"/>
+            </div>
             <div className="">
-                <div className="row p-5">
+                <div className="row p-lg-5">
                     <div className="col-sm shadow-lg rounded-lg" data-aos="fade-down">
                         <img className="img-fluid" src={todoCreate} size="auto" alt="" />
                     </div>
                     <div className="col-sm d-flex
                         justify-content-center align-items-center"data-aos="zoom-in-left">
-                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
+                        <div className="p-lg-5 p-1" style={{borderBottom:"10px solid #0275d8", borderRight:"10px solid #0275d8", borderBottomRightRadius: 40}}>
                             <h1 className="display-4 text-left">Assign tasks to individual team members</h1>
                         </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="row p-5">
+                <hr className="m-5 bg-dark"/>
+            </div>
+            <div>
+                <div className="row p-lg-5">
                     <div className="col-sm shadow-lg rounded-lg"  data-aos="fade-up" data-aos-delay="700" >
                         <img className="img-fluid" src={todoDetailAssigned} size="auto" alt="" />
                     </div>
                     <div className="col-sm d-flex
                         justify-content-center align-items-center order-lg-first" data-aos="zoom-in" data-aos-delay="700">
-                        <div className="p-5" style={{borderBottom:"10px solid #0275d8"}}>
+                        <div className="p-lg-5 p-1" style={{borderBottom:"10px solid #0275d8", borderLeft:"10px solid #0275d8", borderBottomLeftRadius: 40}}>
                             <h1 className="display-4 text-left">Team member who has been assigned task can discuss with the team leader about the todo assigned.</h1>
                         </div>
                     </div>
                 </div>
             </div>
+            <div>
+                <hr className="m-5 bg-dark"/>
+            </div>
             <div className="pt-5">
-                <div className="text-left">
+                <div className="text-left p-lg-5">
                     <h1>This application is in its beta version(0.1.0).</h1>
                     <h1>Following are few update that would be included in future beta versions.</h1>
                     <ul className="p-3">
@@ -139,11 +144,14 @@ const Section1 = () => {
                         <li><h3>Improve in local notification and server notifications.</h3></li>
                         <li><h3>Bugs fix and imprive stability.</h3></li>
                     </ul>
+            <div>
+                <hr className="m-5 bg-dark"/>
+            </div>
                     <div className="pt-5 pb-5">
                         <h2>Report any bugs or crashes, your review on this application 
                             is helpfull for me to provide you with more efficiant and stable application.</h2>
                     </div>
-                    <div className="p-5 text-white bg-danger rounded">
+                    <div className="p-5 text-white bg-danger rounded-pill">
                         <h1>Warning!</h1>
                         <h4>This application is in its beta version, your account, data present in this app may or
                             may not be deleted, however prior notifications will be provided before any changes.
@@ -152,6 +160,9 @@ const Section1 = () => {
                         <h4>It is recommended to enter valid email address, it will be helpfull for me to reach the users and solve the bugs faced.</h4>
                         <h4>Users may need have to create new account for this application on final release</h4>
                     </div>
+            <div>
+                <hr className="m-5 bg-dark"/>
+            </div>
                     <div className="pt-5 pb-5 d-flex justify-content-center align-items-center">
                     <Button color="primary" onClick={toggle}><h1 className="display-4">Download</h1></Button>
                     </div>
